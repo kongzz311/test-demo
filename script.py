237e77aaf96b739b47097933d93b38cf1ac52c7c
kongzz311 <kozenzei@outlook.com>
2019-12-28 22:14:57 +0800

    remove some code

--- script.py
+++ script.py
@@ -8,15 +8,6 @@ print(sys.version)
 print(sys.executable)
 
 
-def greet(who_to_greet):
-
-    greeting = 'hello, {}'.format(who_to_greet)
-    return greeting
-
-
-print(greet('world'))
-print(greet('kongzz'))
-
 # r = requests.get('http://kongzhenzhou.com')
 # print(r.status_code)
 
